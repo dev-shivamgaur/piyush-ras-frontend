aa55879471785b504ec2a4e40205501b2d6d86ef
Shivam <dev.shivamgaur@gmail.com>
2026-05-17 21:10:13 +0530

    Update KavitaCard component styles: change background color, remove background image, and adjust text size for better readability.

--- src/components/MainKavitaCard/KavitaCard.jsx
+++ src/components/MainKavitaCard/KavitaCard.jsx
@@ -281,10 +281,10 @@ const KavitaCard = () => {
       </Helmet>
     <div className="min-h-screen  flex justify-center ">
       <div
-        className="w-full    overflow-hidden relative"
+        className="w-full   bg-[#D7B78D] overflow-hidden relative"
         style={{
           backgroundImage:
-            "url(https://wallpapers.com/images/hd/parchment-paper-background-c7kcurktuicyc12c.jpg)",
+            "",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
@@ -331,7 +331,7 @@ const KavitaCard = () => {
 
 
           <div className="text-gray-800 text-lg sm:text-2xl mt-2 leading-9 space-y-2  font-medium">
-            <pre className="whitespace-pre-wrap font-['Ranga'] text-lg sm:text-xl md:text-2xl leading-relaxed tracking-wide"
+            <pre className="whitespace-pre-wrap font-['Ranga'] text-2xl sm:text-xl md:text-2xl leading-relaxed tracking-wide"
               onCopy={(e) => (e.preventDefault(
                 setError("Copy can not allowed")
               ))}
